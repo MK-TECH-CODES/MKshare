3148080db64245a619c8bd9c174d4cba59907bcf
MK-TECH-CODES <mukeshkumar14000@gmail.com>
2024-03-29 20:01:19 +0530

    mk-share

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
 const express = require("express");
 const connectdb = require("./config/db");
 const path = require("path");
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Middleware
+const PORT = 3500 || process.env.PORT;
+connectdb();
 app.use(express.static("public"));
-app.use(express.json());
+app.use(express.json);
 
-// Template engine
-app.set("views", path.join(__dirname, "views"));
+//Template engine
+app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
-
-// Routes
+// Router
 app.use("/api/files", require("./routes/files"));
 app.use("/files", require("./routes/show"));
+// Download
 app.use("/files/download", require("./routes/download"));
 
-// Start the server after connecting to MongoDB
-async function startServer() {
-  try {
-    connectdb();
-    // MongoDB connection successful, start the server
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
-  }
-}
-
-startServer();
+// For the server listening
+app.listen(PORT, () => {
+  console.log(`Server is running in the ${PORT}`);
+});
