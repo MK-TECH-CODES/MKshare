72ea9f8f2bc8fc15c49d64c0e04c888af17f10c8
MK-TECH-CODES <mukeshkumar14000@gmail.com>
2024-03-29 19:43:10 +0530

    mk-share

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
 const express = require("express");
 const connectdb = require("./config/db");
 const path = require("path");
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000; // Adjust the order to prioritize environment variables
+// Middleware
+app.use(express.static("public"));
+app.use(express.json());
 
-// Connect to MongoDB
-connectdb(() => {
-  // MongoDB connection successful, start the server
-  app.use(express.static("public"));
-  app.use(express.json());
+// Template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-  // Template engine
-  app.set("views", path.join(__dirname, "/views"));
-  app.set("view engine", "ejs");
+// Routes
+app.use("/api/files", require("./routes/files"));
+app.use("/files", require("./routes/show"));
+app.use("/files/download", require("./routes/download"));
 
-  // Routes
-  app.use("/api/files", require("./routes/files"));
-  app.use("/files", require("./routes/show"));
-  app.use("/files/download", require("./routes/download"));
+// Start the server after connecting to MongoDB
+async function startServer() {
+  try {
+    await connectdb();
+    // MongoDB connection successful, start the server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
+}
 
-  // Start the server
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-});
+startServer();
