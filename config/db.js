0bf4f913961923cd9eba39215420c26f24fa1285
MK-TECH-CODES <mukeshkumar14000@gmail.com>
2024-03-29 19:49:47 +0530

    mk-share

--- config/db.js
+++ config/db.js
@@ -3,15 +3,20 @@ const mongoose = require("mongoose");
 
 // Database connection
 function connectdb() {
-  mongoose.connect(process.env.MONGO_CONNECTION_URL, {
-    // Remove deprecated options
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(() => {
-    console.log("Database is connected");
-  }).catch((error) => {
-    console.error("Error connecting to the database:", error);
-  });
+  mongoose
+    .connect(process.env.MONGO_CONNECTION_URL, {
+      // Remove deprecated options
+      useNewUrlParser: true, // Deprecated but still needed for backward compatibility
+      useUnifiedTopology: true, // Deprecated, but should be explicitly set to true
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => {
+      console.log("Database is connected");
+    })
+    .catch((error) => {
+      console.error("Error connecting to the database:", error);
+    });
 }
 
 module.exports = connectdb;
