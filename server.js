73eed70e67d75605ba2d1ae3ed2ce1e64657eada
MK-TECH-CODES <mukeshkumar14000@gmail.com>
2024-03-29 19:17:51 +0530

    mk-share

--- server.js
+++ server.js
@@ -3,21 +3,25 @@ const connectdb = require("./config/db");
 const path = require("path");
 const app = express();
 
-const PORT = 3500 || process.env.PORT;
-connectdb();
-app.use(express.static("public"));
-app.use(express.json())
+const PORT = process.env.PORT || 3500; // Adjust the order to prioritize environment variables
 
-//Template engine
-app.set("views", path.join(__dirname, "/views"));
-app.set("view engine", "ejs");
-// Router
-app.use("/api/files", require("./routes/files"));
-app.use("/files", require("./routes/show"));
-// Download
-app.use("/files/download", require("./routes/download"));
+// Connect to MongoDB
+connectdb(() => {
+  // MongoDB connection successful, start the server
+  app.use(express.static("public"));
+  app.use(express.json());
 
-// For the server listening
-app.listen(PORT, () => {
-  console.log(`Server is running in the ${PORT}`);
+  // Template engine
+  app.set("views", path.join(__dirname, "/views"));
+  app.set("view engine", "ejs");
+
+  // Routes
+  app.use("/api/files", require("./routes/files"));
+  app.use("/files", require("./routes/show"));
+  app.use("/files/download", require("./routes/download"));
+
+  // Start the server
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
